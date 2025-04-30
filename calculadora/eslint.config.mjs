ad7e847b115389ddf192549f2e67f35f19943dc5
Dylan1410 <120534485+Dylan1410@users.noreply.github.com>
2025-04-30 14:49:53 -0500

    2

--- calculadora/eslint.config.mjs
+++ calculadora/eslint.config.mjs
@@ -1,4 +1,4 @@
-// @ts-check
+// @ts-nocheck
 import eslint from '@eslint/js';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 import globals from 'globals';
@@ -28,7 +28,10 @@ export default tseslint.config(
     rules: {
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn'
+      '@typescript-eslint/no-unsafe-argument': 'warn',
+      'no-console': [
+        "error", { allow: ["warn", "error"] }
+      ]
     },
   },
 );
