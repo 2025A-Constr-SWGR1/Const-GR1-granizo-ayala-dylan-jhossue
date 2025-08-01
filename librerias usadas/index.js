a2a4d9fea3581f0b4f03e0690b200e94031ff23b
Dylan1410 <dylan.granizo@epn.edu.ec>
2025-07-31 20:20:57 -0500

    Librerias para la division y multiplicacion

---
+++ librerias usadas/index.js
@@ -0,0 +1,12 @@
+const paqueteDivision= require('2025a-swgr1-djga-division');
+
+const respuesta = paqueteDivision.division(9, 3);
+
+console.log('La respuesta de la division es: ', respuesta);
+
+
+const paqueteMultiplicacion= require('2025a-swgr1-djga-multiplicacion');
+
+const respuestaMultiplicacion = paqueteMultiplicacion.multiplicacion(2, 5);
+
+console.log('La respuesta de la multiplicación es: ', respuestaMultiplicacion);
